5e157f851efe251971e9e44faf2caa11d6657d78
Muhamad Visat Sutarno <mvisat@gmail.com>
2017-06-12 20:06:01 +0700

    edit constants to use old c define instead of global memory

--- src/constants.cuh
+++ src/constants.cuh
@@ -4,24 +4,24 @@
 #include <cmath>
 
 #define EPS 1e-9f
-__constant__ const float M_2PI = M_PI * 2;
+#define M_2PI (M_PI * 2)
 
 #define MAX_MINUTIAE 256
 
 #define R 70
-__constant__ const int R_SQR = R * R;
+#define R_SQR (R * R)
 #define NS 16
 #define ND 6
-__constant__ const int NC = NS * NS * ND;
-__constant__ const float SIGMA_S = 28.0f / 3;
-__constant__ const float SIGMA_S_SQR = SIGMA_S * SIGMA_S;
-__constant__ const float SIGMA_2S_SQR = 2 * SIGMA_S_SQR;
+#define NC (NS * NS * ND)
+#define SIGMA_S (28.0f / 3)
+#define SIGMA_S_SQR (SIGMA_S * SIGMA_S)
+#define SIGMA_2S_SQR (2 * SIGMA_S_SQR)
 #define SIGMA_3S 28
 #define SIGMA_9S_SQR 784 // = (28/3)^2 * 9
-__constant__ const float SIGMA_D = 2 * M_PI / 9;
-__constant__ const float DELTA_S = (float)(R * 2) / NS;
-__constant__ const float DELTA_D = M_PI * 2 / ND;
-__constant__ const float DELTA_D_2 = DELTA_D / 2;
+#define SIGMA_D (2 * M_PI / 9)
+#define DELTA_S (2 * R / NS)
+#define DELTA_D (2 * M_PI / ND)
+#define DELTA_D_2 (DELTA_D / 2)
 #define MU_PSI 0.01f
 #define SIGMA 50
 #define MIN_VC 0.75f
