6b73ba509b7d5af95b616e54792c3732f102dade
Muhamad Visat Sutarno <mvisat@gmail.com>
2017-06-19 19:18:52 +0700

    update main to use argv

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
 #include <fstream>
 #include <vector>
 #include <iostream>
@@ -14,58 +15,73 @@
 using namespace std;
 
 void buildTemplateFromFile(
-    const char *fileName,
-    vector<char>& cylinderValidities,
-    vector<char>& cellValues,
-    vector<char>& cellValidities) {
+    const char *input,
+    const char *output) {
   int width, height, dpi, n;
-  ifstream stream(fileName);
-  stream >> width >> height >> dpi >> n;
+  ifstream istream(input);
+  istream >> width >> height >> dpi >> n;
   vector<Minutia> minutiae;
   for (int i = 0; i < n; ++i) {
     int x, y;
     float theta;
-    stream >> x >> y >> theta;
+    istream >> x >> y >> theta;
     minutiae.emplace_back(x, y, theta);
   }
+  istream.close();
+
+  vector<char> cylinderValidities, cellValidities, cellValues;
 
   auto area = buildValidArea(minutiae, width, height);
   buildTemplate(minutiae, area, width, height,
-    cylinderValidities, cellValues, cellValidities);
+    cylinderValidities, cellValidities, cellValues);
   handleError(cudaDeviceSynchronize());
+
+  ofstream ostream(output);
+  ostream << width << endl;
+  ostream << height << endl;
+  ostream << dpi << endl;
+  ostream << n << endl;
+  for (auto &m: minutiae)
+    ostream << m.x << ' ' << m.y << ' ' << m.theta << endl;
+  ostream << n << endl;
+  for (int i = 0; i < n; ++i) {
+    if (cylinderValidities[i]) {
+      ostream << "True ";
+      for (int j = 0; j < NS; ++j) {
+        for (int k = 0; k < NS; ++k) {
+          ostream << (cellValidities[i*NC + j*NS*ND + k*ND] ? "1 " : "0 ");
+        }
+      }
+      for (int j = 0; j < NC; ++j) {
+        ostream << (cellValues[i*NC + j] ? '1' : '0');
+        if (j != NC-1) ostream << ' ';
+      }
+    } else {
+      ostream << "False";
+    }
+    ostream << endl;
+  }
+  ostream.close();
 }
 
-int main() {
-  vector<char> cellValues1, cellValidities1, cylinderValidities1;
-  buildTemplateFromFile(
-    "data/1_1.txt", cylinderValidities1, cellValidities1, cellValues1);
+void printUsage(char const *argv[]) {
+  cerr << "usage: " << argv[0] << " [template|match] [options]\n";
+  cerr << endl;
+  cerr << "template\t: <input> <output>\n";
+  cerr << "match\t\t: <template1> <template2>\n";
+}
 
-  debug("Cylinder validities:\n");
-  for (int i = 0; i < cylinderValidities1.size(); ++i)
-    debug("%d ", cylinderValidities1[i]);
-  debug("\n\n");
-
-  debug("Cell validities:\n");
-  for (int i = 0; i < NS; ++i) {
-    for (int j = 0; j < NS; ++j) {
-      debug("%d ", cellValidities1[2*NC + i*NS*ND + j*ND]);
+int main(int argc, char const *argv[]) {
+  if (argc > 1) {
+    if (strncmp(argv[1], "template", 8) == 0 && argc == 4) {
+      buildTemplateFromFile(argv[2], argv[3]);
+      return 0;
+    } else if (strncmp(argv[1], "match", 5) == 0 && argc == 4) {
+      // TODO
+      return 0;
     }
   }
-  debug("\n\n");
 
-  debug("Cell values:\n");
-  for (int i = 0; i < NC; ++i)
-    debug("%d ", cellValues1[2*NC + i]);
-  debug("\n\n");
-
-  // vector<char> cellValues2, cellValidities2, cylinderValidities2;
-  // buildTemplateFromFile(
-  //   "data/1_2.txt", cylinderValidities2, cellValidities2, cellValues2);
-  //
-  // auto globalScore = matchTemplate(
-  //   cylinderValidities1, cellValidities2, cellValues1,
-  //   cylinderValidities2, cellValidities1, cellValues2);
-  // debug("Global score: %f\n", globalScore);
-
-  return 0;
+  printUsage(argv);
+  return 1;
 }
