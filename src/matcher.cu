a689c3a1aefa87f1f6c6cc21915ed2f12b6ffa7f
Muhamad Visat Sutarno <mvisat@gmail.com>
2017-06-21 14:24:26 +0700

    matcher: fix wrong grid dimension

--- src/matcher.cu
+++ src/matcher.cu
@@ -87,7 +87,6 @@ float LSS(const vector<float>& _matrix, int rows, int cols) {
     return 1.0f / (1.0f + expf(-tau * (value-mu)));
   };
   int n = MIN_NP + roundf(sigmoid(min(rows, cols), TAU_P, MU_P) * (MAX_NP - MIN_NP));
-  debug("NP: %d\n", n);
   nth_element(matrix.begin(), matrix.begin()+n, matrix.end(), greater<float>());
   float sum = 0.0f;
   for (int i = 0; i < n; ++i)
@@ -185,7 +184,7 @@ float matchTemplate(
     cudaMalloc(&devMatrix, devMatrixSize));
 
   int threadPerDim = 32;
-  dim3 blockCount(ceilMod(rows, threadPerDim), ceilMod(cols, threadPerDim));
+  dim3 blockCount(ceilMod(cols, threadPerDim), ceilMod(rows, threadPerDim));
   dim3 threadCount(threadPerDim, threadPerDim);
   computeSimilarity<<<blockCount, threadCount>>>(
     devMinutiae1, devCylinderValidities1, devBinarizedValidities1, devBinarizedValues1,
